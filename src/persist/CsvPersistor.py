f7fe5f1e740e7c2f58104f1e3d4abe9f0274cc7c
juanpabosu <jbsuaza@gmail.com>
2019-04-09 22:13:23 -0500

    Ajuste estacion y hora a persistor
    Se modifican el modo de escritura para los campos region, estacion y hora, ademas se ajusta cambios de linea en blanco en el write csv

--- src/persist/CsvPersistor.py
+++ src/persist/CsvPersistor.py
@@ -1,6 +1,5 @@
 import csv
 
-
 class CsvPersistor:
 
     def __init__(self, region_tables, output_file):
@@ -8,25 +7,25 @@ class CsvPersistor:
         self.output_file = output_file
 
     def persist(self):
-        print 'Persistiendo datos en CSV de paginas de regiones'
+        print ('Persistiendo datos en CSV de paginas de regiones')
         self.__persist_header__()
         for region_table in self.region_tables:
-            region = region_table.region.encode('utf-8')
-            hora = region_table.hora.encode('utf-8')
-            estacion = region_table.estacion.encode('utf-8')
+            region = region_table.region
             for region_row in region_table.region_rows:
-                self.__persist_row__(region, hora, estacion, region_row)
+                self.__persist_row__(region,region_row)
 
-    def __persist_row__(self, region, hora, estacion, region_row):
-        ICA = region_row.ICA.encode('utf-8')
-        O3 = region_row.O3.encode('utf-8')
-        NO2 = region_row.NO2.encode('utf-8')
-        SO2 = region_row.SO2.encode('utf-8')
-        pm25 = region_row.pm25.encode('utf-8')
-        pm10 = region_row.pm10.encode('utf-8')
-        co = region_row.co.encode('utf-8')
+    def __persist_row__(self, region,region_row):
+        ICA = region_row.ICA
+        hora=  region_row.hora
+        estacion=  region_row.estacion
+        O3 = region_row.O3
+        NO2 = region_row.NO2
+        SO2 = region_row.SO2
+        pm25 = region_row.pm25
+        pm10 = region_row.pm10
+        co = region_row.co
         data = [region, hora, estacion, ICA, O3, NO2, SO2, pm25, pm10, co]
-        with open("../data/" + self.output_file, 'a') as csv_file:
+        with open("../data/" + self.output_file, 'a', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(data)
         csv_file.close()
@@ -34,7 +33,8 @@ class CsvPersistor:
     
     def __persist_header__(self):
         data = ['region', 'hora_publicacion', 'estacion', 'ica', 'o3', 'no2', 'so2', 'pm25', 'pm10', 'co']
-        with open("../data/" + self.output_file, 'a') as csv_file:
+        with open("../data/" + self.output_file, 'a', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(data)
         csv_file.close()
+
